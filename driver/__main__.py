d29f5a9f7f78921880700d0babe737ce0e86303d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-03-17 21:39:30 -0400

    Upgrade analysis and write blog

--- driver/__main__.py
+++ driver/__main__.py
@@ -2,9 +2,9 @@ from .functions import request_func, selenium_func
 from .parallel import serial, multithread, multiprocess
 import json
 
-N = 10
+N = 100
 
 for func in (request_func, selenium_func):
   for par in (multiprocess, serial, multithread):
-    for result in par(func, 10):
+    for result in par(func, N):
       print json.dumps(result)
--- driver/parallel.py
+++ driver/parallel.py
@@ -1,12 +1,14 @@
 from multiprocessing.pool import Pool, ThreadPool
 
+M = 10
+
 def serial(func, n):
   results = map(func, range(n))
   for r in results:
     r["parallel"] = "serial"
   return results
 
-def multithread(func, n, m=3):
+def multithread(func, n, m=M):
   pool = ThreadPool(m)
   results = pool.map(func, range(n))
   pool.close()
@@ -14,7 +16,7 @@ def multithread(func, n, m=3):
     r["parallel"] = "multithread"
   return results
 
-def multiprocess(func, n, m=3):
+def multiprocess(func, n, m=M):
   pool = Pool(m)
   results = pool.map(func, range(n))
   pool.close()
